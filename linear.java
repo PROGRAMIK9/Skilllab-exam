7a7415bb3cfbc69f027e3d61cf3f89ac1a4c4e71
divyanth <divyanthgowdas.mca25@rvce.edu.in>
2026-03-03 15:33:15 +0530

    added

---
+++ linear.java
@@ -0,0 +1,8 @@
+Scanner sc =new Scanner(system.in);
+System.out.println("Enter number of ele:");
+int n=sc.nextInt();
+int arr[]=new int[n];
+System.out.println("Enter" + n + " elements:");
+for (int i=0; i< n; i++){
+arr[i]  sc.nextInt();
+
