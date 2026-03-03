33938f18fccde85a1f2e7ab5c83c8a47b9825877
Aaryamann Prabhu <140898184+PROGRAMIK9@users.noreply.github.com>
2026-03-03 15:47:34 +0530

    FInalMerge

--- linear.java
+++ linear.java
@@ -1,7 +1,20 @@
-Scanner sc =new Scanner(system.in);
+
+import java.util.Scanner;
+class Main{
+	public static void main(String args[]){
+    Scanner sc =new Scanner(system.in);
 System.out.println("Enter number of ele:");
 int n=sc.nextInt();
 int arr[]=new int[n];
 System.out.println("Enter" + n + " elements:");
 for (int i=0; i< n; i++){
 arr[i]  sc.nextInt();
+	System.out.println("Enter a number to search: ");
+		int k = Integer.valueOf(sc.nextLine());
+		for (int i = 0; i < 5; i++) {
+			if (arr[i] == k) {
+				System.out.println("Number not found in array");	
+			}
+		}
+	}
+}
